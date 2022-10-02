92d2a7805af4327b18b235512c8be2f393f7d48e
akshayd2020 <63755431+akshayd2020@users.noreply.github.com>
2022-10-02 09:05:12 -0400

    Fix bot to send messages

--- deal.py
+++ deal.py
@@ -1,6 +1,7 @@
 import os
 
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
 def main():
@@ -8,22 +9,33 @@ def main():
     TOKEN = os.getenv('DISCORD_TOKEN')
     GUILD = os.getenv('DISCORD_GUILD')
 
-    client = discord.Client(intents=discord.Intents.default())
+    description = '''Bot to emote based on command text'''
+    intents = discord.Intents.all()
+    bot = commands.Bot(command_prefix='/', description=description, intents=intents)
+    # client = discord.Client(intents=discord.Intents.all())
 
-    @client.event
+    @bot.event
     async def on_ready():
-        print(f'{client.user} has connected to Discord!')
+        print(f'{bot.user} has connected to Discord!')
 
-        guild = discord.utils.get(client.guilds, name=GUILD)
+        for guild in bot.guilds:
+            if guild.name == GUILD:
+                break
 
         print(
-            f'{client.user} is connected to the following guild:\n'
+            f'{bot.user} is connected to the following guild:\n'
             f'{guild.name}(id: {guild.id})'
         )
+    @bot.event
+    async def on_message(message):
+        print(f'{message.content}\n')
+        await bot.process_commands(message)
 
+    @bot.command()
+    async def emote(ctx, *, arg):
+        await ctx.send(arg)
 
-
-    client.run(TOKEN)
+    bot.run(TOKEN)
 
 if __name__ == '__main__':
     main()
